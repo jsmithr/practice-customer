236d3e62de3d037b7c0db563dfaabfd78571b2a5
jsmithr <johannsmiithr@gmail.com>
2021-10-08 13:04:52 -0500

    add photo-service

--- photo-service/src/main/java/pragma/photoservice/PhotoServiceApplication.java
+++ photo-service/src/main/java/pragma/photoservice/PhotoServiceApplication.java
@@ -2,8 +2,14 @@ package pragma.photoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableEurekaClient
+//@EnableMongoRepositories
 @SpringBootApplication
+//@EnableMongoAuditing
 public class PhotoServiceApplication {
 
 	public static void main(String[] args) {
